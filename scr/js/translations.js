392d0351bfeeebc1d57f9d0dd895b90527d915c8
Trịnh Hg <onepiecechapter@gmail.com>
2025-07-01 21:34:18 +0700

    Update translations.js

--- scr/js/translations.js
+++ scr/js/translations.js
@@ -1,1 +1,16 @@
+// src/js/translations.js
+(function () {
+  window.App = window.App || {};
 
+  App.translations = {
+    vn: {
+      appTitle: 'Tiện Ích Của Trịnh Hg',
+      contactText1: '- Gia hạn tài khoản: ',
+      settingsTab: 'Settings',
+      replaceTab: 'Replace',
+      splitTab: 'Chia Chương',
+      // ... (giữ nguyên toàn bộ translations object từ code gốc)
+      reloadButton: 'Tải lại'
+    }
+  };
+})();
